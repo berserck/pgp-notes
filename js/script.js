3425bf13b72b1edf22abb17777738b741ae82b58
Pedro Parracho <pedro.parracho@gmail.com>
2018-06-16 11:29:52 +0200

    Move code to dedicated js and css files. Add background and footer.

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const notesForm = document.querySelector('.note-form');
+const notesArea = document.querySelector('.notes');
+const notes = JSON.parse(localStorage.getItem('notes')) || [];
+
+function addNote(e){
+    e.preventDefault();
+    const note = (this.querySelector('[name=new-note]')).value;
+    notes.push(note);
+    populateList(notes, notesArea);
+    window.localStorage.setItem('notes', JSON.stringify(notes));
+    this.reset();
+}
+
+function populateList(notes = [], notesList){
+    notesList.innerHTML = notes.map((note, i) => {
+        return `
+<li>
+<label for = "note${i}"> ${note}</label>
+</li>
+`;
+    }).join('');
+}
+
+notesForm.addEventListener('submit', addNote);
+populateList(notes, notesArea);
